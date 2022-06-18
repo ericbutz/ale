006a81f5a69db1a2c646d05570ed46cbdd9f81e1
Eric Butz <ericgbutz@gmail.com>
2022-06-18 14:51:44 -0600

    Add new nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,3 +1,68 @@
+import React from 'react';
+
+class ContextMenu extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  handleMenuClicked = (event) => {
+    event.preventDefault()
+    this.props.onMenuClicked()
+  }
+  
+  render() {
+    const {isMenuOpened} = this.props
+    return(
+      <>
+          <nav className={isMenuOpened ? 'menu' :'hideMenu'}>
+            <a href="#features">Features</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#testimonials">Testimonials</a>
+            <a href="#team">Team</a>
+            <a href="#contact">Contact</a>
+          </nav>
+         
+        <div onClick={this.handleMenuClicked} className='contextMenu'/>
+      </>
+    )
+  }
+}
+
+export class Navigation extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isMenuOpened: false
+    }
+  }
+  
+  onMenuClicked = () => {
+    this.setState({
+      isMenuOpened: !this.state.isMenuOpened
+    })
+  }
+  
+  render() {
+    const {isMenuOpened} = this.state
+    return (
+      //<div className='scrollingContainer'>
+      //  <div className='section' id='1'>SECTION 1</div>
+      //  <div className='section' id='2'>SECTION 2</div>
+      //  <div className='section' id='3'>SECTION 3</div>
+      //  <div className='section' id='4'>SECTION 4</div>
+      //  <div className='floating'>
+          <ContextMenu isMenuOpened={isMenuOpened} onMenuClicked={this.onMenuClicked}/>
+      //  </div>
+      //</div>
+    )
+  }
+}
+
+
+
+/*
 export const Navigation = (props) => {
   return (
     <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
@@ -66,3 +131,6 @@ export const Navigation = (props) => {
     </nav>
   )
 }
+*/
+
+//export default Navigation;
